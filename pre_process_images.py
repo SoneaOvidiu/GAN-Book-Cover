8197b9e8af215041da1d6792b76b00a7b5f2971f
maram <maram@DESKTOP-7OHI1BV>
2020-11-18 20:24:22 +0200

    Pre-processed images added

--- pre_process_images.py
+++ pre_process_images.py
@@ -23,9 +23,11 @@ for category_directory in categories:
     if not exists(discarded_category_path):
         mkdir(discarded_category_path)
 
-    for input_image in listdir(input_category_path):
-        input_image_path = join(input_category_path, input_image)
-        output_image_path = join(output_category_path, input_image)
-        discarded_image_path = join(discarded_category_path, input_image)
+    for input_file in listdir(input_category_path):
+        if not input_file.endswith(".csv"):
+            input_image = input_file
+            input_image_path = join(input_category_path, input_image)
+            output_image_path = join(output_category_path, input_image)
+            discarded_image_path = join(discarded_category_path, input_image)
 
-        remove_text_from_image(input_image_path, output_image_path, discarded_image_path)
+            remove_text_from_image(input_image_path, output_image_path, discarded_image_path)
